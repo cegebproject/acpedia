7b7b6ab224cbe7e0e4f13e7c161320213e545f37
cegebproject <todlahbacot@gmail.com>
2026-01-05 14:12:04 +0700

    blank

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,9 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        $data = [
-            'title' => 'Dashboard | ACPedia'
-        ];
-        return view('welcome_message');
+        return '';
     }
 }
